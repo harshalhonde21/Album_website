42e7ec1a51bbb93758f6115476b07dfaaf170ad1
harshalhonde21 <harshalhonde17@gmail.com>
2023-12-05 21:37:56 +0530

    updated frontend

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,20 +1,25 @@
-  import { Fragment } from "react";
-  import "../Css/Home.css";
+import { Fragment } from "react";
+import "../Css/Home.css";
 
-  const Home = () => {
-    return (
-      <Fragment>
-        <div className="home-container">
-          <div className="image-section">
-            <div className="overlay-text">
-              <h1>Creating Memories <br />Together</h1>
-              <button>Click Me</button>
-            </div>
-            <img src="/photobooks.webp" alt="Background" />
+const Home = () => {
+  return (
+    <Fragment>
+      <div className="home-container">
+        <div className="text-section">
+          <div className="overlay-text">
+            <h1>Prism Photography</h1>
+            <br />  
+            <br />  
+            <h3>Every moment, every emotion, perfectly framed.</h3>
+            <button>Explore</button>
           </div>
         </div>
-      </Fragment>
-    );
-  };
+        <div className="image-section">
+          <img src="/bg1.jpg" alt="Background" />
+        </div>
+      </div>
+    </Fragment>
+  );
+};
 
-  export default Home;
+export default Home;
